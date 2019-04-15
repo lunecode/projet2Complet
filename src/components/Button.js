03b17d0441a161489d965b31b92d6fab9a800745
QuietMonkey <taillandierbertrand@gmail.com>
2019-04-15 11:18:56 +0200

    change button logo & style

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Button.css'
+import Loader from './Loader'
 
-const Button = ({fonction}) => {
+const Button = ( {fonction}) => {
+   // const modifyButton = 'button' + {category}
     return (
-      <div>
-        <button className='button' onClick={fonction}><span className='fuck'>FUCK</span> <span className='it'>IT !</span></button>
-        <figure/>
-    </div>)
+     <div className='loading'>
+        <button className='button' onClick={fonction}><div className='picto'></div></button>
+        <Loader />
+     </div>)
 }
 
 export default Button;
