9bdab0caa13993856d03aa86311f59170a8302e5
QuietMonkey <taillandierbertrand@gmail.com>
2019-05-01 16:48:02 +0200

    Collapse OK - Style : Minimalist

--- front/src/components/CardComponent.js
+++ front/src/components/CardComponent.js
@@ -1,40 +1,46 @@
 import React, { Component } from 'react';
 import "./CardComponent.css";
-import {Collapse} from 'react-collapse';
+import { Collapse } from 'react-collapse';
 import Excuse from "./Excuse";
 import DetailsAlert from "./DetailsAlert";
 import CardCTA from './CardCTA'
 
 
 
-  class CardComponent extends Component {
-    state = {open:false,};
-    // componentWillUnmount() {
-    //   this.setState({open: false})
-    // }
+class CardComponent extends Component {
+  state = {
+    open: false,
+    comp: 'cardClose'
+  };
+  // componentWillUnmount() {
+  //   this.setState({open: false})
+  // }
 
 
-    toToggle = () => {
-      this.setState({open: !this.state.open})
-      console.log(this.state)
+  toToggle = () => {
+    this.setState({ open: !this.state.open })
+    if (this.state.comp === 'cardClose') {
+      this.setState({ comp: 'cardOpen' })
+    } else {
+      this.setState({ comp: 'cardClose' })
     }
+    console.log(this.state.comp)
+  }
 
-    render () {
-    return (<div>
-          <div className="logoTitleContainer">
-            <img
-              src="https://www.retif.eu/media/image/400x/picto-028-pole-de-correspondance-ou-gare-routiere-pvc-fond-blanc-125x125mm_01.jpg"
-              alt="title img"
-            />
-            <h2>{this.props.dataTitle}</h2>
-          <i className={"fas fa-chevron-down down-arrow"} onClick={this.toToggle}/>
-          </div>
-          <Collapse isOpened={this.state.open} className="collapse">
-      <div className="CardWrapper">
-        <button className="back-btn" onClick={this.props.goBack}>
-          <i className="fas fa-arrow-left back-logo" />Retour
-        </button>
-        <div className="CardComponent">
+
+  render() {
+    return (
+      <div className='cardComponent'>
+        <div className='headerCard' onClick={this.toToggle}>
+          <img
+            src="https://www.retif.eu/media/image/400x/picto-028-pole-de-correspondance-ou-gare-routiere-pvc-fond-blanc-125x125mm_01.jpg"
+            alt="title img"
+          />
+          <h2>{this.props.dataTitle}</h2>
+        </div>
+
+        <Collapse isOpened={this.state.open} className="collapse">
+
           <DetailsAlert
             picture={this.props.dataLogo}
             details="Détails de l'incident"
@@ -52,15 +58,61 @@ import CardCTA from './CardCTA'
             <i
               className={"fas fa-chevron-right right-arrow"}
               onClick={this.props.toNext}
-              />
+            />
           </div>
-        </div>
-        
+
+        </Collapse>
       </div>
-              </Collapse>
-              </div>
-    );
-    }
+
+    )
+  }
 }
+
+    
+//     render () {
+//     return (
+    
+//     <div className={this.state.comp}>
+//           <div className="logoTitleContainer">
+//             <img
+//               src="https://www.retif.eu/media/image/400x/picto-028-pole-de-correspondance-ou-gare-routiere-pvc-fond-blanc-125x125mm_01.jpg"
+//               alt="title img"
+//             />
+//             <h2>{this.props.dataTitle}</h2>
+//           <i className={"fas fa-chevron-down down-arrow"} onClick={this.toToggle}/>
+//           </div>
+//           <Collapse isOpened={this.state.open} className="collapse">
+//       <div className="CardWrapper">
+//         <button className="back-btn" onClick={this.props.goBack}>
+//           <i className="fas fa-arrow-left back-logo" />Retour
+//         </button>
+//         <div>
+//           <DetailsAlert
+//             picture={this.props.dataLogo}
+//             details="Détails de l'incident"
+//             lastTime={this.props.dataLastTime}
+//             time={this.props.dataTime}
+//             issue={this.props.dataIssue}
+//           />
+//           <Excuse excuse={this.props.dataExcuse} />
+//           <div className="Card-CTA">
+//             <i
+//               className={"fas fa-chevron-left left-arrow"}
+//               onClick={this.props.toPrevious}
+//             />
+//             <CardCTA dataCopy={this.props.dataExcuse} />
+//             <i
+//               className={"fas fa-chevron-right right-arrow"}
+//               onClick={this.props.toNext}
+//               />
+//           </div>
+//         </div>
+        
+//       </div>
+//               </Collapse>
+//               </div>
+//     );
+//     }
+// }
 
 export default CardComponent;
